48f2ce54e47ee18b7fc030912939c1bfcaa478c4
hjwnu <tlstjr01@gmail.com>
2024-06-07 13:04:46 +0900

    fix config

---
+++ src/test/java/org/nullcom/nullcomnullog/NullcomNullogApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nullcom.nullcomnullog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NullcomNullogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
